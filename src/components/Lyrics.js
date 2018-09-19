48bc192ca7f739724cb88a75a45df6a81f7345f0
Rohit Kumar <rohit77275@gmail.com>
2018-09-19 17:10:39 +0530

    add api key for testing

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -14,14 +14,14 @@ class Lyrics extends Component {
       .get(
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
           this.props.match.params.id
-        }&apikey=${process.env.REACT_APP_MM_KEY}`
+        }&apikey=93a20d99999177eae580f060a8c6cd8d`
       )
       .then(res => {
         this.setState({ lyrics: res.data.message.body.lyrics });
         return axios.get(
           `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${
             this.props.match.params.id
-          }&apikey=${process.env.REACT_APP_MM_KEY}`
+          }&apikey=93a20d99999177eae580f060a8c6cd8d`
         );
       })
       .then(res => this.setState({ track: res.data.message.body.track }))
--- src/store/actions/trackList.js
+++ src/store/actions/trackList.js
@@ -5,9 +5,7 @@ export const getTracks = () => dispatch => {
   dispatch(setLoading());
   axios
     .get(
-      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=in&f_has_lyrics=1&apikey=${
-        process.env.REACT_APP_MM_KEY
-      }`
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=in&f_has_lyrics=1&apikey=93a20d99999177eae580f060a8c6cd8d`
     )
     .then(res =>
       dispatch({
@@ -22,9 +20,7 @@ export const updateTracks = track_title => dispatch => {
   dispatch(setLoading());
   axios
     .get(
-      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${track_title}&page_size=10&page=1&s_track_rating=desc&apikey=${
-        process.env.REACT_APP_MM_KEY
-      }`
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${track_title}&page_size=10&page=1&s_track_rating=desc&apikey=93a20d99999177eae580f060a8c6cd8d`
     )
     .then(res =>
       dispatch({
